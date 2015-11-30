c3f866f54442fdfd7babf33514a298f1707f69d7
IlyaAvdeev <ilya.avdeev@smartbear.com>
2015-11-30 13:13:44 +0300

    "In JIRA settings, add text "example: https://company.atlassian.net" after the URL field" requirement implementation

--- src/main/java/com/smartbear/ready/plugin/jira/factories/JiraPrefsFactory.java
+++ src/main/java/com/smartbear/ready/plugin/jira/factories/JiraPrefsFactory.java
@@ -12,15 +12,19 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 @PluginPrefs
 public class JiraPrefsFactory implements Prefs {
     public static final String BUG_TRACKER_LOGIN = "Username:";
     public static final String BUG_TRACKER_PASSWORD = "Password:";
     public static final String BUG_TRACKER_LOGIN_DESCRIPTION = "Your JIRA user account (not an email)";
+    public static final String BUG_TRACKER_LOGIN_IN_FIELD_DESCRIPTION = "Your JIRA user account (not an email)";
     public static final String BUG_TRACKER_PASSWORD_DESCRIPTION = "The password for logging in";
     public static final String BUG_TRACKER_URL = "JIRA server URL:";
     public static final String BUG_TRACKER_URL_DESCRIPTION = "The URL of your JIRA instance, for instance, https://mycompany.atlassian.net";
+    public static final String BUG_TRACKER_URL_IN_FIELD_DESCRIPTION = "The URL of your JIRA instance (https://...)";
     public static final String JIRA_PREFS_TITLE = "JIRA";
 
     private SimpleForm form;
@@ -43,6 +47,48 @@ public class JiraPrefsFactory implements Prefs {
         }
     }
 
+    private class BugTrackerUrlFieldFocusListener implements FocusListener {
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            JTextField bugTrackerUrl = (JTextField)e.getSource();
+            if (bugTrackerUrl.getText() != null &&
+                    bugTrackerUrl.getText().equals(BUG_TRACKER_URL_IN_FIELD_DESCRIPTION)) {
+                bugTrackerUrl.setText("");
+            }
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            JTextField bugTrackerUrl = (JTextField)e.getSource();
+            if (bugTrackerUrl.getText() != null &&
+                    bugTrackerUrl.getText().equals("")) {
+                bugTrackerUrl.setText(BUG_TRACKER_URL_IN_FIELD_DESCRIPTION);
+            }
+        }
+    }
+
+    private class BugTrackerLoginFieldFocusListener implements FocusListener {
+
+        @Override
+        public void focusGained(FocusEvent e) {
+            JTextField loginField = (JTextField)e.getSource();
+            if (loginField.getText() != null &&
+                    loginField.getText().equals(BUG_TRACKER_LOGIN_IN_FIELD_DESCRIPTION)) {
+                loginField.setText("");
+            }
+        }
+
+        @Override
+        public void focusLost(FocusEvent e) {
+            JTextField loginField = (JTextField)e.getSource();
+            if (loginField.getText() != null &&
+                    loginField.getText().equals("")) {
+                loginField.setText(BUG_TRACKER_LOGIN_IN_FIELD_DESCRIPTION);
+            }
+        }
+    }
+
     @Override
     public SimpleForm getForm() {
         if (form == null) {
@@ -50,10 +96,12 @@ public class JiraPrefsFactory implements Prefs {
             form.addSpace();
             JTextField loginField = form.appendTextField(BUG_TRACKER_LOGIN, BUG_TRACKER_LOGIN_DESCRIPTION);
             loginField.getDocument().addDocumentListener(new BugTrackerSettingsChangeListener());
+            loginField.addFocusListener(new BugTrackerLoginFieldFocusListener());
             JPasswordField passwordField = form.appendPasswordField(BUG_TRACKER_PASSWORD, BUG_TRACKER_PASSWORD_DESCRIPTION);
             passwordField.getDocument().addDocumentListener(new BugTrackerSettingsChangeListener());
             JTextField bugTrackerUrl = form.appendTextField(BUG_TRACKER_URL, BUG_TRACKER_URL_DESCRIPTION);
             bugTrackerUrl.getDocument().addDocumentListener(new BugTrackerSettingsChangeListener());
+            bugTrackerUrl.addFocusListener(new BugTrackerUrlFieldFocusListener());
         }
 
         return form;
@@ -73,17 +121,21 @@ public class JiraPrefsFactory implements Prefs {
 
     @Override
     public void storeValues(StringToStringMap values, Settings settings) {
-        settings.setString(BugTrackerPrefs.LOGIN, values.get(BUG_TRACKER_LOGIN));
+        if (values.get(BUG_TRACKER_LOGIN) != null && values.get(BUG_TRACKER_LOGIN).equals(BUG_TRACKER_LOGIN_IN_FIELD_DESCRIPTION)) {
+            settings.setString(BugTrackerPrefs.LOGIN, values.get(BUG_TRACKER_LOGIN));
+        }
         settings.setString(BugTrackerPrefs.PASSWORD, values.get(BUG_TRACKER_PASSWORD));
-        settings.setString(BugTrackerPrefs.DEFAULT_URL, values.get(BUG_TRACKER_URL));
+        if (values.get(BUG_TRACKER_URL) != null && values.get(BUG_TRACKER_URL).equals(BUG_TRACKER_URL_IN_FIELD_DESCRIPTION)) {
+            settings.setString(BugTrackerPrefs.DEFAULT_URL, values.get(BUG_TRACKER_URL));
+        }
     }
 
     @Override
     public StringToStringMap getValues(Settings settings) {
         StringToStringMap values = new StringToStringMap();
-        values.put(BUG_TRACKER_LOGIN, settings.getString(BugTrackerPrefs.LOGIN, ""));
+        values.put(BUG_TRACKER_LOGIN, settings.getString(BugTrackerPrefs.LOGIN, BUG_TRACKER_LOGIN_IN_FIELD_DESCRIPTION));
         values.put(BUG_TRACKER_PASSWORD, settings.getString(BugTrackerPrefs.PASSWORD, ""));
-        values.put(BUG_TRACKER_URL, settings.getString(BugTrackerPrefs.DEFAULT_URL, ""));
+        values.put(BUG_TRACKER_URL, settings.getString(BugTrackerPrefs.DEFAULT_URL, BUG_TRACKER_URL_IN_FIELD_DESCRIPTION));
         return values;
     }
 
