6f3486a66332db036345e6584de044f3af4b37be
Ilya Avdeev <avdeeff.ilya@gmail.com>
2020-11-26 18:31:25 +0300

    Supporting newer Java style

--- src/main/java/com/smartbear/ready/plugin/jira/settings/BugTrackerPrefs.java
+++ src/main/java/com/smartbear/ready/plugin/jira/settings/BugTrackerPrefs.java
@@ -5,15 +5,15 @@ import com.smartbear.ready.plugin.jira.factories.JiraPrefsFactory;
 
 public interface BugTrackerPrefs {
     @Setting(name = JiraPrefsFactory.BUG_TRACKER_URL, description = JiraPrefsFactory.BUG_TRACKER_URL_DESCRIPTION)
-    public final static String DEFAULT_URL = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-default-url";
+    String DEFAULT_URL = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-default-url";
 
     @Setting(name = JiraPrefsFactory.BUG_TRACKER_LOGIN, description = JiraPrefsFactory.BUG_TRACKER_LOGIN_DESCRIPTION)
-    public final static String LOGIN = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-login";
+    String LOGIN = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-login";
 
     @Setting(name = JiraPrefsFactory.BUG_TRACKER_PASSWORD, description = JiraPrefsFactory.BUG_TRACKER_PASSWORD_DESCRIPTION)
-    public final static String PASSWORD = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-password";
+    String PASSWORD = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-password";
 
     @Setting(name = JiraPrefsFactory.SKIP_RELEASED_VERSIONS, description = JiraPrefsFactory.SKIP_RELEASED_VERSIONS_DESCRIPTION)
-    public final static String SKIP_VERSIONS = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-skip-released-versions";
+    String SKIP_VERSIONS = BugTrackerPrefs.class.getSimpleName() + "@" + "jira-skip-released-versions";
 }
 
