d17909d59b983e756e76c7e30c231ee3ab2e0e15
Son Vu <son.vu@smartbear.com>
2022-08-12 13:13:55 +0700

    update to 5.2.4

--- src/main/java/com/smartbear/ready/plugin/jira/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/jira/PluginConfig.java
@@ -5,7 +5,7 @@ import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 
 @PluginConfiguration(groupId = "com.smartbear.ready.plugins",
-        name = "JIRA Integration Plugin", version = "1.6.3",
+        name = "JIRA Integration Plugin", version = "1.6.4",
         autoDetect = true, description = "Creates JIRA items for failed tests directly from the ReadyAPI IDE.",
         infoUrl = "https://github.com/smartbear/ready-api-plugin-jira",
         minimumReadyApiVersion = "3.20.1")
