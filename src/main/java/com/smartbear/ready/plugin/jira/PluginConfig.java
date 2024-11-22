78bc928b2fb922918cbd1881fe7415512f3df693
Rafał Szpak <rafal.szpak@smartbear.com>
2024-11-22 20:20:40 +0100

    RIA-23469 Adjust libraries versions
    - bump ReadyAPI to 3.57.0
    - replace javax api with jakarta api: jsr311-api -> jakarta.ws.rs-api
    - implement UriBuilder converting javax -> jakarta: jira-rest-java-client-core bases on javax types
    - bundle transitive jettison 1.3.7 which was removed from ReadyAPI
    - transitive jettison has security vulnerabilities - subject to bump to 1.5.4 if compatible

--- src/main/java/com/smartbear/ready/plugin/jira/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/jira/PluginConfig.java
@@ -8,7 +8,7 @@ import com.eviware.soapui.plugins.PluginConfiguration;
         name = "JIRA Integration Plugin", version = "1.6.6",
         autoDetect = true, description = "Creates JIRA items for failed tests directly from the ReadyAPI IDE.",
         infoUrl = "https://github.com/SmartBear/ready-jira-plugin/wiki",
-        minimumReadyApiVersion = "3.49.0")
+        minimumReadyApiVersion = "3.57.0")
 public class PluginConfig extends PluginAdapter {
     @Override
     public boolean isActive() {
---
+++ src/main/java/javax/ws/rs/core/UriBuilder.java
@@ -0,0 +1,30 @@
+package javax.ws.rs.core;
+
+import java.net.URI;
+
+public class UriBuilder {
+    private jakarta.ws.rs.core.UriBuilder internalUriBuilder;
+
+    protected UriBuilder() {
+    }
+
+    public static UriBuilder fromUri(URI uri) {
+        UriBuilder instance = new UriBuilder();
+        instance.internalUriBuilder = jakarta.ws.rs.core.UriBuilder.fromUri(uri);
+        return instance;
+    }
+
+    public UriBuilder path(String path) {
+        internalUriBuilder.path(path);
+        return this;
+    }
+
+    public UriBuilder queryParam(String name, Object... values) {
+        internalUriBuilder.queryParam(name, values);
+        return this;
+    }
+
+    public URI build(Object... values) {
+        return internalUriBuilder.build(values);
+    }
+}
