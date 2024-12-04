a89e52baf1da907629df9d5437c74794075ad086
Marharyta Panska <marharyta.panska@smartbear.com>
2024-12-04 12:43:41 +0100

    RIA-23619 Bump plugin version after release

--- src/main/java/com/smartbear/ready/plugin/jira/PluginConfig.java
+++ src/main/java/com/smartbear/ready/plugin/jira/PluginConfig.java
@@ -5,7 +5,7 @@ import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 
 @PluginConfiguration(groupId = "com.smartbear.ready.plugins",
-        name = "JIRA Integration Plugin", version = "1.6.7",
+        name = "JIRA Integration Plugin", version = "1.6.8-SNAPSHOT",
         autoDetect = true, description = "Creates JIRA items for failed tests directly from the ReadyAPI IDE.",
         infoUrl = "https://github.com/SmartBear/ready-jira-plugin/wiki",
         minimumReadyApiVersion = "3.57.0")
